13db74222edec4589fde3ffc094c62e2ac122cb7
Emerson-Duarte <pduarte.emerson@gmail.com>
2020-09-13 18:39:15 -0300

    pages created and styles to her

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,52 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(items_params)
+
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to contacts_path, notice: 'Sucesso' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+
+    respond_to do |format|
+      if @contact.update(items_params)
+        format.html { redirect_to contacts_path, notice: 'Sucesso' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+
+    respond_to do |format|
+      format.html { redirect_to contacts_path, notice: 'Foi deletado com sucesso!'}
+    end
+  end
+
+  private
+
+  def items_params
+    params.require(:contact).permit(:name, :address, :phone)
+  end
+end
